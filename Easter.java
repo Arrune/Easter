30770a2580aedb7246bf8ced15f6d6c27b925f85
Arrune <alpachay45@gmail.com>
2025-09-04 20:49:39 +0000

    Version 3

--- Easter.java
+++ Easter.java
@@ -10,13 +10,13 @@ public class Easter {
         int e = b%4;
         int f = (b+8)/25;
         int g = (b-f+1)/3;
-        int h = ((19*a)+b-d-g+15)/30;
+        int h = (19*a+b-d-g+15)%30;
         int i = c/4;
         int k = c%4;
         int r = (32+2*e+2*i-h-k)%7;
         int m = (a+11*h+22*r)/451;
-        int n = (h+r-7*5+114)/31;
-        int p = (h+r-7*5+114)%31;
+        int n = (h+r-7*m+114)/31;
+        int p = (h+r-7*m+114)%31;
 
         System.out.println("a = " +a);
         System.out.println("b = " +b);
@@ -32,10 +32,5 @@ public class Easter {
         System.out.println("m = " +m);
         System.out.println("n = " +n);
         System.out.println("p = " +p);
-        
-
-
-
-
     }
 }
