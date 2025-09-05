9427a5fb21d9e8a098f37f0d0a5f2724dbffc8ad
Arrune <alpachay45@gmail.com>
2025-09-05 17:05:53 +0000

    Submission

--- Easter.java
+++ Easter.java
@@ -1,8 +1,8 @@
 // Arrune Nimalan
-// This program will calculate the date Easter falls on given a year after 1583
+// This program will calculate the date Easter falls on given a year after 2003
 public class Easter {
     public static void main(String[] args) {
-        int y = 1583;
+        int y = 2003;
         int a = y%19;
         int b = y/100;
         int c = y%100;
